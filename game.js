64e628db45a3f7426362422591c4f7d4b3368421
Santiago Iglesias <santiigle07@gmail.com>
2023-07-10 00:14:27 -0300

    agrego clases

--- game.js
+++ game.js
@@ -12,16 +12,16 @@ document.addEventListener('DOMContentLoaded', () => {
       const buyLinks = gameDetails.stores.map(store => `<a href="${store.url ==""? "https://"+store.store.domain:store.url}" target="_blank">${store.store.name}</a>`).join(', ');
   
       gameResult.innerHTML = `
-        <img src=${gameDetails.background_image}></img>
-        <h3>${gameDetails.name}</h3>
-        <p>Description: ${gameDetails.description_raw}</p>
-        <p>Genre: ${gameDetails.genres.map(genre => genre.name).join(', ')}</p>
-        <p>Platforms: ${gameDetails.platforms.map(platform => platform.platform.name).join(', ')}</p>
-        <p>Release Date: ${gameDetails.released}</p>
-        <p>Publisher: ${gameDetails.publishers.map(publisher => publisher.name).join(', ')}</p>
-        <p>Age Rating: ${ageRating}</p>
-        <p>Buy: ${buyLinks}</p>
-        <a href="${gameDetails.website}">Website</a>
+        <img src=${gameDetails.background_image} class= "imagen-juego"></img>
+        <h3 class= "gameName">${gameDetails.name}</h3>
+        <p class= "description">Description: ${gameDetails.description_raw}</p>
+        <p class= "genre">Genre: ${gameDetails.genres.map(genre => genre.name).join(', ')}</p>
+        <p class= "platforms">Platforms: ${gameDetails.platforms.map(platform => platform.platform.name).join(', ')}</p>
+        <p class= "released">Release Date: ${gameDetails.released}</p>
+        <p class= "publisher">Publisher: ${gameDetails.publishers.map(publisher => publisher.name).join(', ')}</p>
+        <p class= "ageRating">Age Rating: ${ageRating}</p>
+        <p class= "buyLinks">Buy: ${buyLinks}</p>
+        <a href="${gameDetails.website}" class= "website">Website</a>
       `;
     } else {
       gameResult.innerHTML = 'No se encontró información del juego.';
