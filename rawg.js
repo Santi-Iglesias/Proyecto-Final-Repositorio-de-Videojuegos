b0805b218d8e348a4945e098e9d48c09b1f467e4
Santiago Iglesias <santiigle07@gmail.com>
2023-07-09 22:52:55 -0300

    modificacion extraccion de juegos

--- rawg.js
+++ rawg.js
@@ -1,4 +1,5 @@
 const API_KEY = import.meta.env.VITE_RAWG_API_KEY;
+const PAGE_SIZE = 40;
 
 export async function getGameDetails(gameId) {
   const response = await fetch(`https://api.rawg.io/api/games/${gameId}?key=${API_KEY}`);
@@ -8,14 +9,14 @@ export async function getGameDetails(gameId) {
 }
 
 export async function getTrendingGames() {
-  const response = await fetch(`https://api.rawg.io/api/games?key=${API_KEY}&ordering=-added`);
+  const response = await fetch(`https://api.rawg.io/api/games?key=${API_KEY}&ordering=-added&page_size=${PAGE_SIZE}`);
   const data = await response.json();
 
   return data;
 }
 
 export async function searchGameByName(name) {
-  const response = await fetch(`https://api.rawg.io/api/games?key=${API_KEY}&search=${encodeURIComponent(name)}`);
+  const response = await fetch(`https://api.rawg.io/api/games?key=${API_KEY}&search=${encodeURIComponent(name)}&page_size=${PAGE_SIZE}`);
   const data = await response.json();
 
   return data;
